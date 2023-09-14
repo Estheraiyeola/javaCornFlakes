f2f295299543ffe907570fcaa8656367b9a778d7
Estheraiyeola <estheraiyeola@yahoo.com>
2023-09-14 02:27:26 +0100

    Made some modifications on the Drivers class

--- src/bankApp/Main.java
+++ src/bankApp/Main.java
@@ -14,7 +14,7 @@ public class Main {
         mainMenu();
     }
     public static void mainMenu(){
-        System.out.print("""
+        String message = ("""
                 =====================================================================
                                 WELCOME TO ESTHER'S BANK
                 =====================================================================
@@ -26,160 +26,158 @@ public class Main {
                 5. CHECK BALANCE
                 0. EXIT
                 """);
-        String userInput = input.next();
-        if (userInput.equals("1")) createAccount();
-        else if (userInput.equals("2")) deposit();
-        else if (userInput.equals("3")) withdraw();
-        else if (userInput.equals("4")) transfer();
-        else if (userInput.equals("5")) checkBalance();
-        else if (userInput.equals("0")){
-            System.out.println("BYE!!!");
-            exit(0);
-        }
-        else{
-            System.out.println("Invalid option");
-            mainMenu();
+        String userInput = input(message);
+        switch (userInput) {
+            case "1" -> createAccount();
+            case "2" -> deposit();
+            case "3" -> withdraw();
+            case "4" -> transfer();
+            case "5" -> checkBalance();
+            case "0" -> {
+                System.out.println("BYE!!!");
+                exit(0);
+            }
+            default -> {
+                System.out.println("Invalid option");
+                mainMenu();
+            }
         }
     }
 
     private static void checkBalance() {
-        System.out.println("Enter your account number: ");
-        String accountNumber = validateAccountNumber();
-        String pin = validatePin();
-        int balance = bank.checkBalance(accountNumber, pin);
-        System.out.println("BALANCE IS " + balance);
-        goBackToMain();
-        mainMenu();
+        try {
+            String accountNumber = input("Enter your account number: ");
+            String pin = input("Enter your pin: ");
+            int balance = bank.checkBalance(accountNumber, pin);
+            display("BALANCE IS " + balance);
+            goBackToMain();
+            mainMenu();
+        }catch (IllegalArgumentException | IllegalStateException e){
+            display(e.getMessage());
+            mainMenu();
+        }
+
     }
     public static void getAccountNumber(){
-        System.out.print("""
+        display("""
                 ======================================================
                         GET YOUR ACCOUNT NUMBER
                 ======================================================
-                """);
+                ACCOUNT NUMBER -> \s""" + bank.getSize());
     }
 
     private static void transfer() {
-        System.out.println("How much do you want to transfer: ");
-        int amountToBeTransferred = input.nextInt();
-        System.out.println("What is your account number: ");
-        String sendersAccountNumber = validateAccountNumber();
-        System.out.println("What is the receivers account number: ");
-        String receiversAccountNumber = validateAccountNumber();
-        String sendersPin = validatePin();
-        bank.transfer(amountToBeTransferred, sendersAccountNumber, receiversAccountNumber, sendersPin);
-        System.out.println("TRANSFER SUCCESSFUL!!!");
-        goBackToMain();
-        mainMenu();
+        try {
+            int amountToBeTransferred = Integer.parseInt(input("How much do you want to transfer: "));
+            String sendersAccountNumber = input("What is your account number: ");
+            String receiversAccountNumber = input("What is the receivers account number: ");
+            String sendersPin = input("Enter your pin: ");
+            bank.transfer(amountToBeTransferred, sendersAccountNumber, receiversAccountNumber, sendersPin);
+            display("TRANSFER SUCCESSFUL!!!");
+            goBackToMain();
+            mainMenu();
+        }catch (InputMismatchException | IllegalStateException | IllegalArgumentException e){
+            display(e.getMessage());
+            mainMenu();
+        }
+
     }
 
 
     private static void withdraw() {
         try {
-            System.out.println("How much do you want to withdraw: ");
-            int amountToWithdraw = input.nextInt();
-            System.out.println("Account number please: ");
-            String accountNumber = validateAccountNumber();
-            String pin = validatePin();
+            int amountToWithdraw = Integer.parseInt(input("How much do you want to withdraw: "));
+            String accountNumber = input("Account number please: ");
+            String pin = input("Enter your pin: ");
             bank.withdraw(amountToWithdraw, accountNumber, pin);
-        }catch(IllegalStateException e){
-            System.out.println("Account not found");
-            validateAccountNumber();
-        }catch (IllegalArgumentException e){
-            System.out.println("Incorrect pin");
-            validatePin();
+            System.out.println("TAKE YOUR CASH!!!");
+            goBackToMain();
+            mainMenu();
+        }catch(IllegalStateException | IllegalArgumentException | InputMismatchException e) {
+            display(e.getMessage());
+            mainMenu();
         }
-        System.out.println("TAKE YOUR CASH!!!");
-        goBackToMain();
-        mainMenu();
-    }
-
-    private static String validatePin() {
-        try {
-            System.out.println("Enter your 4 digit pin: ");
-            String pin = input.next();
-            return pin;
-        }catch (IllegalArgumentException e){
-            System.out.println("Invalid pin");
-            validatePin();
-        }
-        return null;
 
     }
 
-    private static String validateAccountNumber() {
-        try {
-            String accountNumber = input.next();
-            return accountNumber;
-        }catch (IllegalStateException e){
-            System.out.println("Account not found");
-            String accountNumber = input.next();
-            validateAccountNumber();
-        }
-        return null;
+//    private static String validatePin() {
+//        try {
+//            System.out.println("Enter your 4 digit pin: ");
+//            String pin = input.next();
+//            return pin;
+//        }catch (IllegalArgumentException e){
+//            System.out.println("Invalid pin");
+//            validatePin();
+//        }
+//        return null;
+//
+//    }
 
-    }
-
-    private static int validateAmount(int amount) {
-        try {
-            amount = input.nextInt();
-        }
-        catch (InputMismatchException e){
-            System.out.println("Invalid input");
-            validateAmount(amount);
-        }
-        return amount;
-    }
+//    private static String validateAccountNumber() {
+//        try {
+//            String accountNumber = input.next();
+//            return accountNumber;
+//        }catch (IllegalStateException e){
+//            System.out.println("Account not found");
+//            String accountNumber = input.next();
+//            validateAccountNumber();
+//        }
+//        return null;
+//
+//    }
+//
+//    private static int validateAmount(int amount) {
+//        try {
+//            amount = input.nextInt();
+//        }
+//        catch (InputMismatchException e){
+//            System.out.println("Invalid input");
+//            validateAmount(amount);
+//        }
+//        return amount;
+//    }
 
     private static void deposit() {
         try {
-            System.out.println("How much do uou want to deposit? ");
-            int amountToBeDeposited = input.nextInt();
-            System.out.println("Account number please: ");
-            String accountNumber = validateAccountNumber();
+            int amountToBeDeposited = Integer.parseInt(input("How much do uou want to deposit? "));
+            String accountNumber = input("Account number please: ");
             bank.deposit(amountToBeDeposited, accountNumber);
+            display("Amount Successfully deposited");
+            goBackToMain();
+            mainMenu();
         }catch (InputMismatchException | IllegalArgumentException | IllegalStateException e){
             System.out.println("Invalid Input");
             deposit();
         }
-
-
-        System.out.println("""
-                DEPOSIT WAS SUCCESSFUL!!!
-                """);
-        goBackToMain();
-        mainMenu();
     }
 
     private static void goBackToMain() {
-        System.out.println("PRESS 0 TO GO BACK TO MAIN MENU");
-        String userInput = input.next();
+        String userInput = input("PRESS 0 TO GO BACK TO MAIN MENU");
         if (userInput.equals("0")) mainMenu();
-        else System.out.println("Invalid Option. Press 0 to continue");
+        else display("Invalid Option. Press 0 to continue");
     }
 
     public static void createAccount(){
-        System.out.println("Enter your first name: ");
-        String firstName = input.next();
-        System.out.println("Enter your last name: ");
-        String lastName = input.next();
         try {
-            String pin = validatePin();
+            String firstName = input("Enter your first name: ");
+            String lastName = input("Enter your last name: ");
+            String pin = input("Enter your pin: ");
             bank.register(firstName, lastName, pin);
+            getAccountNumber();
+            display("ACCOUNT CREATED SUCCESSFULLY");
+            goBackToMain();
+            mainMenu();
         }
-        catch (IllegalArgumentException e){
-            System.out.println("Invalid Pin");
-            validatePin();
+        catch (IllegalArgumentException | InputMismatchException e){
+            display(e.getMessage());
+            mainMenu();
         }
-        getAccountNumber();
-        System.out.println("ACCOUNT CREATED SUCCESSFULLY");
-        goBackToMain();
-        mainMenu();
+
     }
     public static void display(String message){
         JOptionPane.showMessageDialog(null, message);
     }
-    public static void input(String message){
-        JOptionPane.showInputDialog(message);
+    public static String input(String message){
+        return JOptionPane.showInputDialog(null,message);
     }
 }
